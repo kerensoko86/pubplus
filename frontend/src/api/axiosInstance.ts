62803a16f2df70e89f43f56b8d79208be0ce7c70
kerensoko86 <keren.sokoletsky@gmail.com>
2023-12-07 09:14:42 +0200

    remove comments

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -8,7 +8,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("authToken"); // Assuming you store the token in localStorage
+    const token = localStorage.getItem("authToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
